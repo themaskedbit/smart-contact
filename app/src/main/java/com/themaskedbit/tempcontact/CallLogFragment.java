d050e58087a1c844d84141c3578068d788f691bf
rohanreji93 <rohanreji93@gmail.com>
2019-02-17 15:30:09 +0530

    Changed class names

--- app/src/main/java/com/themaskedbit/tempcontact/CallLogFragment.java
+++ app/src/main/java/com/themaskedbit/tempcontact/CallLogFragment.java
@@ -5,33 +5,21 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CallLog;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentContainer;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.telecom.Call;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static android.support.v4.content.PermissionChecker.checkSelfPermission;
@@ -52,7 +40,7 @@ public class CallLogFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     private List<Contact> contactList = new ArrayList<>();
     private RecyclerView recyclerView;
-    private ContactsAdapter mAdapter;
+    private CallLogAdapter mAdapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -117,7 +105,7 @@ public class CallLogFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         recyclerView = (RecyclerView) getActivity().findViewById(R.id.recycler_view);
 
-        mAdapter = new ContactsAdapter(contactList);
+        mAdapter = new CallLogAdapter(contactList);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
